7af980e077a6d106d78a5a5d4df5357a5b22d8cb
SHRESHTH18 <shreshth18062005@gmail.com>
2025-03-26 02:41:20 +0530

    majority_element(>n/3)

--- Striver_A2Z/src/step3_Arrays/lec3_Hard/p1_pascals_triangle.java
+++ Striver_A2Z/src/step3_Arrays/lec3_Hard/p1_pascals_triangle.java
@@ -10,30 +10,78 @@ public class p1_pascals_triangle {
         triangle = generate(n);
         System.out.println(triangle);
     }
+
+    /*
+    6th row of pascals triangle - 1 5 10 10 5 1
+    first and last element always 1
+    then 5/1 , 5*4 / 1*2 ,  5*4*3 / 1*2*3 , 5*4*3*2 / 1*2*3*4
+    multiplying by (row - col) / col
+     */
     public static ArrayList<ArrayList<Integer>> generate(int numRows){
         ArrayList<ArrayList<Integer>> triangle= new ArrayList<>();
         for(int i = 1;i<=numRows;i++){
             triangle.add(new ArrayList<>());
         }
+        // for nth row , use a for loop on column and iterate over R of NCR
         for(int i=0;i<numRows;i++){
-            if(i==0){
-                triangle.get(0).add(1);
-            }
-            else if(i==1){
-                triangle.get(1).add(1);
-                triangle.get(1).add(1);
-            }
-            else{
-                triangle.get(i).add(1);
-                int k=0;
-                for(int j=1;j<i;j++){
-                    triangle.get(i).add(triangle.get(i-1).get(k)+triangle.get(i-1).get(k+1));
-                    k++;
-                }
-                triangle.get(i).add(1);
+            triangle.get(i).add(1);
+            int ans=1;
+            for(int j=0;j<i;j++){
+                ans=ans*(i-j);
+                ans=ans/(j+1);
+                triangle.get(i).add(ans);
             }
         }
         return triangle;
     }
+
+    //not good to calculate factorial .
+//    public static int factorial(int n){
+//        int fact=1;
+//        for(int i=2;i<=n;i++){
+//            fact*= i;
+//        }
+//        return fact;
+//    }
+
+    // for any element ,element = (n-1) C (r-1) = n!/ [r! * (n-r)!]
+    public static int NCR(int n,int r){
+        int res=1;
+        for(int i=0;i<r;i++){
+            res=res * (n-i);
+            res=res/(i+1);
+        }
+        return res;
+    }
+    /*
+    approach
+    saw the pattern ki first and last element =1
+    for middle elements upar wali row ka koi to 2 element(relation nikal lo) ka sum
+     */
+//    public static ArrayList<ArrayList<Integer>> generate(int numRows){
+//        ArrayList<ArrayList<Integer>> triangle= new ArrayList<>();
+//        for(int i = 1;i<=numRows;i++){
+//            triangle.add(new ArrayList<>());
+//        }
+//        for(int i=0;i<numRows;i++){
+//            if(i==0){
+//                triangle.get(0).add(1);
+//            }
+//            else if(i==1){
+//                triangle.get(1).add(1);
+//                triangle.get(1).add(1);
+//            }
+//            else{
+//                triangle.get(i).add(1);
+//                int k=0;
+//                for(int j=1;j<i;j++){
+//                    triangle.get(i).add(triangle.get(i-1).get(k)+triangle.get(i-1).get(k+1));
+//                    k++;
+//                }
+//                triangle.get(i).add(1);
+//            }
+//        }
+//        return triangle;
+//    }
 
 }
--- Striver_A2Z/src/step3_Arrays/lec3_Hard/p2_majority_element.java
+++ Striver_A2Z/src/step3_Arrays/lec3_Hard/p2_majority_element.java
@@ -16,15 +16,72 @@ public class p2_majority_element {
         System.out.println(list);
     }
 
+
+
+    /*
+
+    approach - max 2 elements can have count>n/3
+    dono ka count rkhlo
+    if count 0 h to set element and count =1
+    fir same element to count++
+    agar dono elements se match nahi kr rha to dono ka count--;
+
+     */
+
     public static ArrayList<Integer> majorityElement(int[] nums){
         ArrayList<Integer> list = new ArrayList<>();
+        int c1=0,c2=0;
+        int el1=0,el2=0;
         for(int i=0;i<nums.length;i++){
-            
+            if(c1==0 && el2!=nums[i]){
+                el1=nums[i];
+                c1=1;
+            }
+            else if(c2==0 && el1!= nums[i]) {
+                el2=nums[i];
+                c2=1;
+            }
+            else if(nums[i]==el1){
+                c1++;
+            }
+
+
+            else if(nums[i]==el2){
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+
+
+        //just manually verifying the answer
+
+        int count1=0;
+        int count2=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==el1){
+                count1++;
+            }
+            else if(nums[i]==el2){
+                count2++;
+            }
+        }
+        if(count1>nums.length/3){
+            list.add(el1);
+        }
+        if(count2>nums.length/3){
+            list.add(el2);
         }
         return list;
     }
 
-    //takes a lot of time
+    //takes a lot of time -16 ms
+    /*
+    approach - map me element store krlo with their count ,
+    if element present, count++ else new key value pair add kro (element,1)
+     */
 //    public static ArrayList<Integer> majorityElement(int[] nums){
 //        ArrayList<Integer> list = new ArrayList<>();
 //        HashMap<Integer,Integer> map = new HashMap<>();
@@ -39,6 +96,22 @@ public class p2_majority_element {
 //        for(int a:map.keySet()){
 //            if(map.get(a)>nums.length/3){
 //                list.add(a);
+//            }
+//        }
+//        return list;
+//    }
+    //brute -1436 ms
+//    public static ArrayList<Integer> majorityElement(int[] nums){
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for(int i=0;i<nums.length;i++){
+//            int count=0;
+//            for(int j=0;j<nums.length;j++){
+//                if(nums[i]==nums[j]){
+//                    count+=1;
+//                }
+//            }
+//            if(count>nums.length/3 && !list.contains(nums[i])){
+//                list.add(nums[i]);
 //            }
 //        }
 //        return list;
