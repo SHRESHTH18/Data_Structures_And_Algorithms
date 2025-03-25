f8523ae9c01d3ce7e11afe1a7d693d45a47307d6
SHRESHTH18 <shreshth18062005@gmail.com>
2025-03-25 22:58:30 +0530

    some new approaches

--- Striver_A2Z/src/step3_Arrays/lec2_Medium/p12_rotate_matrix_90.java
+++ Striver_A2Z/src/step3_Arrays/lec2_Medium/p12_rotate_matrix_90.java
@@ -1,10 +1,8 @@
 package step3_Arrays.lec2_Medium;
-/*
-the first row of rotated matrix is first column of original matrix bottom to up
- */
+
 import java.util.Arrays;
 import java.util.Scanner;
-public class p12_rotate_matrix_90 {
+public class  p12_rotate_matrix_90 {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
 
@@ -22,18 +20,52 @@ public class p12_rotate_matrix_90 {
             System.out.println(Arrays.toString(matrix[i]));
         }
     }
+
+    /*
+    first transpose the matrix and then reverse every row
+     */
     public static void rotate(int[][] matrix){
         int n=matrix.length;
-        int[][] rotated= new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                rotated[i][j]=matrix[n-j-1][i];
+        //transpose
+        for (int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix,i,j);
             }
         }
+        //reversing each row
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=rotated[i][j];
+            for(int j=0;j<matrix[i].length/2;j++){
+                swap_1D(matrix[i],j,n-j-1);
             }
         }
     }
+    public static void swap(int[][] arr,int i,int j){
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
+    }
+    public static void swap_1D(int [] arr,int i , int j ){
+        int temp=arr[i];
+        arr[i] = arr[j];
+        arr[j]=temp;
+    }
+
+    /*
+the first row of rotated matrix is first column of original matrix bottom to up
+ */
+    //TC- O(n^2) SC-O(n^2)
+//    public static void rotate(int[][] matrix){
+//        int n=matrix.length;
+//        int[][] rotated= new int[n][n];
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                rotated[i][j]=matrix[n-j-1][i];
+//            }
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                matrix[i][j]=rotated[i][j];
+//            }
+//        }
+//    }
 }
--- Striver_A2Z/src/step3_Arrays/lec2_Medium/p13_spiral_matrix.java
+++ Striver_A2Z/src/step3_Arrays/lec2_Medium/p13_spiral_matrix.java
@@ -1,16 +1,6 @@
 package step3_Arrays.lec2_Medium;
-/*
-approach
-draw on pen paper and find out how will the iterator move
-k : loop number
-k=0 outer loop
-k=1 inner loop and so on
-first line : k,k->k,m-k-1
-last column : k+1,m-k-1 -> n-k-1,m-k-1
-last line : n-k-1,m-k-2 -> n-k-1,k
-first column : n-k-1,k->k+1,k
-repeat this all elements are covered
- */
+
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -32,39 +22,91 @@ public class p13_spiral_matrix {
         ArrayList<Integer> list =spiralOrder(matrix);
         System.out.println(list);
     }
+
+    /*
+    approach-keep 4 variables - left right bottom top
+     */
     public static ArrayList<Integer> spiralOrder(int[][] matrix){
         ArrayList<Integer> list= new ArrayList<>();
-        int count=0;
         int n = matrix.length;
         int m= matrix[0].length;
-        int k=0;
-        while(count<m*n){
-            for(int j=k;j<=m-k-1;j++){
-                if(list.size()<m*n){
-                    list.add(matrix[k][j]);
-                    count++;
+        int left=0,right=m-1,top=0,bottom=n-1;
+        while(top<=bottom && left<=right){
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+            right--;
+
+            //if there is one row , do baar print hojaega if we do not apply the "if" condition.
+            // we do this because there were changes in top after entering the while loop.
+            if(top<=bottom){
+                for (int i = right; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
                 }
             }
-            for(int i=k+1;i<=n-k-1;i++){
-                if(list.size()<m*n){
-                    list.add(matrix[i][m-k-1]);
-                    count++;
+            bottom--;
+            //we use the if condition for similar reasons
+            //bcoz value of right was changed after entering the while loop
+            if(left<=right){
+                for (int i = bottom; i >= top; i--) {
+                    list.add(matrix[i][left]);
                 }
             }
-            for(int j=m-k-2;j>=k;j--){
-                if(list.size()<m*n){
-                    list.add(matrix[n-k-1][j]);
-                    count++;
-                }
-            }
-            for(int i=n-k-2;i>=k+1;i--){
-                if(list.size()<m*n){
-                    list.add(matrix[i][k]);
-                    count++;
-                }
-            }
-            k++;
+            left++;
         }
         return list;
     }
+
+
+    /*
+approach
+draw on pen paper and find out how will the iterator move
+k : loop number
+k=0 outer loop
+k=1 inner loop and so on
+first line : k,k->k,m-k-1
+last column : k+1,m-k-1 -> n-k-1,m-k-1
+last line : n-k-1,m-k-2 -> n-k-1,k
+first column : n-k-1,k->k+1,k
+repeat this all elements are covered
+ */
+//    public static ArrayList<Integer> spiralOrder(int[][] matrix){
+//        ArrayList<Integer> list= new ArrayList<>();
+//        int count=0;
+//        int n = matrix.length;
+//        int m= matrix[0].length;
+//        int k=0;
+//        while(count<m*n){
+//            for(int j=k;j<=m-k-1;j++){
+//                if(list.size()<m*n){
+//                    list.add(matrix[k][j]);
+//                    count++;
+//                }
+//            }
+//            for(int i=k+1;i<=n-k-1;i++){
+//                if(list.size()<m*n){
+//                    list.add(matrix[i][m-k-1]);
+//                    count++;
+//                }
+//            }
+//            for(int j=m-k-2;j>=k;j--){
+//                if(list.size()<m*n){
+//                    list.add(matrix[n-k-1][j]);
+//                    count++;
+//                }
+//            }
+//            for(int i=n-k-2;i>=k+1;i--){
+//                if(list.size()<m*n){
+//                    list.add(matrix[i][k]);
+//                    count++;
+//                }
+//            }
+//            k++;
+//        }
+//        return list;
+//    }
 }
