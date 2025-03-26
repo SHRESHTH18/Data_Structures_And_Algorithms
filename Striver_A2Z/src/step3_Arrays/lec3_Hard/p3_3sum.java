bd9ce4c198712b06e1d3d640c7e392e37406d358
SHRESHTH18 <shreshth18062005@gmail.com>
2025-03-26 14:48:47 +0530

    Update p3_3sum.java

--- Striver_A2Z/src/step3_Arrays/lec3_Hard/p3_3sum.java
+++ Striver_A2Z/src/step3_Arrays/lec3_Hard/p3_3sum.java
@@ -16,8 +16,47 @@ public class p3_3sum {
         ArrayList<ArrayList<Integer>> list= threeSum(arr);
         System.out.println(list);
     }
+    /*
+    we use 2 pointer approach
+    1 fixed pointer and 2  moving pointers
+    First, we will sort the entire array.
+We will use a loop(say i) that will run from 0 to n-1. This i will represent the fixed pointer. In each iteration, this value will be fixed for all different values of the rest of the 2 pointers. Inside the loop, we will first check if the current and the previous element is the same and if it is we will do nothing and continue to the next value of i.
+After that, there will be 2 moving pointers i.e. j(starts from i+1) and k(starts from the last index). The pointer j will move forward and the pointer k will move backward until they cross each other while the value of i will be fixed.
+Now we will check the sum i.e. arr[i]+arr[j]+arr[k].
+If the sum is greater, then we need lesser elements and so we will decrease the value of k(i.e. k--).
+If the sum is lesser than the target, we need a bigger value and so we will increase the value of j (i.e. j++).
+If the sum is equal to the target, we will simply insert the triplet i.e. arr[i], arr[j], arr[k] into our answer and move the pointers j and k skipping the duplicate elements(i.e. by checking the adjacent elements while moving the pointers).
+Finally, we will have a list of unique triplets.
+     */
+    public static ArrayList<ArrayList<Integer>> threeSum(int [] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: Sort the array to handle duplicates easily
 
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for 'i' to avoid duplicate triplets
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate elements for 'left' and 'right'
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) left++;  // Move left pointer to increase sum
+                else right--;             // Move right pointer to decrease sum
+            }
+        }
+        return result;
+    }
     /*
     TLE
     approach -
