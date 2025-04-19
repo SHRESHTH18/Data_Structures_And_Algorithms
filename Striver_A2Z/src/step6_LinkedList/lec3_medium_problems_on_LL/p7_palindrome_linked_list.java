b94a4ac4d540e32839814039fc586a60010a44c5
SHRESHTH18 <shreshth18062005@gmail.com>
2025-04-20 01:56:44 +0530

    .

--- Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p7_palindrome_linked_list.java
+++ Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p7_palindrome_linked_list.java
@@ -1,5 +1,35 @@
 package step6_LinkedList.lec3_medium_problems_on_LL;
 
 public class p7_palindrome_linked_list {
-    StringBuilder SB= new StringBuilder();
+    public boolean isPalindrome(ListNode head) {
+        if(head==null ||head.next==null) return true;
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode temp=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode tail=reverseLL(slow);
+        while(tail!=null){
+            if(temp.data!=tail.data){
+                return false;
+            }
+            temp=temp.next;
+            tail=tail.next;
+        }
+        return true;
+    }
+    public ListNode reverseLL(ListNode head){
+        ListNode prevNode=null;
+        ListNode currentNode=head;
+        ListNode nextNode = head;
+        while(nextNode!=null){
+            nextNode=currentNode.next;
+            currentNode.next=prevNode;
+            prevNode=currentNode;
+            currentNode=nextNode;
+        }
+        return prevNode;
+    }
 }
---
+++ Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p8_odd_even_linked_list.java
@@ -0,0 +1,46 @@
+package step6_LinkedList.lec3_medium_problems_on_LL;
+import java.util.ArrayList;
+public class p8_odd_even_linked_list {
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null || head.next.next==null) return head;
+        ListNode odd=head;
+        ListNode even = head.next;
+        ListNode evenHead=even;
+        while(even!=null && even.next!=null ){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+    }
+    
+//    public ListNode oddEvenList(ListNode head) {
+//        if(head==null || head.next==null || head.next.next==null) return head;
+//        ListNode temp=head;
+//        ArrayList<Integer> list= new ArrayList<>();
+//        while(temp!=null && temp.next!=null){
+//            list.add(temp.data);
+//            temp=temp.next.next;
+//        }
+//        if(temp!=null) list.add(temp.data);
+//        if(head.next!=null){
+//            temp=head.next;
+//        }
+//        while(temp!=null && temp.next!=null){
+//            list.add(temp.data);
+//            temp=temp.next.next;
+//        }
+//        if(temp!=null) list.add(temp.data);
+//        temp=head;
+//        int i=0;
+//        while(temp!=null){
+//            temp.data=list.get(i);
+//            i++;
+//            temp=temp.next;
+//        }
+//        return head;
+//    }
+}
