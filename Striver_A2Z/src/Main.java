d2563d37206ac52101c4ca2155b6e81b3584bb49
SHRESHTH18 <shreshth18062005@gmail.com>
2025-04-21 17:19:50 +0530

    ll

--- Striver_A2Z/src/Main.java
+++ Striver_A2Z/src/Main.java
@@ -1,14 +1,31 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.HashSet;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String s1="Test";
-        String s2="Test";
-        System.out.println(s1);
-        System.out.println(s2);
-        StringBuilder sb= new StringBuilder("Hello");
-        sb.insert(2,"java");
-        System.out.println(sb.capacity());
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter length");
+        int n= in.nextInt();
+        System.out.println("enter array");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        System.out.println(numRabbits(arr));
+    }
+    public static int numRabbits(int[] answers) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n=answers.length;
+        for(int i=0;i<n;i++){
+            map.put(answers[i],map.getOrDefault(answers[i],0)+1);
+        }
+        int count=0;
+        for(int key:map.keySet()){
+            int mul=(int)Math.ceil((double)map.get(key)/(double)(key+1));
+
+            count = count+ (1 + key)*mul;
+        }
+        return count;
     }
 }
--- Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p10_sort_LL.java
+++ Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p10_sort_LL.java
@@ -1,8 +1,68 @@
 package step6_LinkedList.lec3_medium_problems_on_LL;
 
 public class p10_sort_LL {
-
-
+    //merge sort
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode middle=findMiddle(head);
+        ListNode leftHead=head;
+        ListNode rightHead=middle.next;
+        middle.next=null;
+        leftHead=sortList(leftHead);
+        rightHead=sortList(rightHead);
+        return merge(leftHead,rightHead);
+    }
+    public ListNode merge(ListNode leftHead,ListNode rightHead){
+        ListNode left=leftHead;
+        ListNode right=rightHead;
+        ListNode head=left;
+        if(left.data<right.data){
+            head = left;
+            left=left.next;
+        }
+        else{
+            head=right;
+            right=right.next;
+        }
+        ListNode temp=head;
+        while(left!=null && right!=null){
+            if(left.data<right.data){
+                temp.next=left;
+                temp=temp.next;
+                left=left.next;
+            }
+            else{
+                temp.next=right;
+                temp=temp.next;
+                right=right.next;
+            }
+        }
+        while(left!=null){
+            temp.next=left;
+            temp=temp.next;
+            left=left.next;
+        }
+        while(right!=null){
+            temp.next=right;
+            temp=temp.next;
+            right=right.next;
+        }
+        return head;
+    }
+    public ListNode findMiddle(ListNode head){
+        if(head==null|| head.next==null) return head;
+        ListNode fast= head.next;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+    
+    
     //selection sort -TLE
 //    public ListNode sortList(ListNode head) {
 //        ListNode temp=head;
