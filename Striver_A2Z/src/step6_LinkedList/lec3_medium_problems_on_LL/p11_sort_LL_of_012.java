3bdfb798544e83d92e72484910fa0bd9e2844751
SHRESHTH18 <shreshth18062005@gmail.com>
2025-04-23 19:44:38 +0530

    intersection_of_YLL using 3 approaches

--- Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p11_sort_LL_of_012.java
+++ Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p11_sort_LL_of_012.java
@@ -3,19 +3,55 @@ package step6_LinkedList.lec3_medium_problems_on_LL;
 public class p11_sort_LL_of_012 {
     public ListNode sortList(ListNode head) {
         ListNode temp=head;
-        int[] count=new int[3];
+        ListNode head0=new ListNode(-1);
+        ListNode head1=new ListNode(-1);
+        ListNode head2=new ListNode(-1);
+        ListNode temp0=head0;
+        ListNode temp1=head1;
+        ListNode temp2=head2;
+
         while(temp!=null){
-            count[temp.data]++;
+            if(temp.data==0){
+                temp0.next=temp;
+                temp0=temp0.next;
+            }
+            else if(temp.data==1){
+                temp1.next=temp;
+                temp1=temp1.next;
+            }
+            else{
+                temp2.next=temp;
+                temp2=temp2.next;
+            }
             temp=temp.next;
         }
-        temp=head;
-        for(int i=0;i<3;i++){
-            while(count[i]>0){
-                temp.data=i;
-                temp=temp.next;
-                count[i]--;
-            }
+        if (head1.next != null) {
+            temp0.next = head1.next;
+        } else {
+            temp0.next = head2.next;
         }
-        return head;
+        temp1.next=head2.next;
+        temp2.next=null;
+        ListNode newHead=head0.next;
+        return newHead;
     }
+
+    //O(2N)
+//    public ListNode sortList(ListNode head) {
+//        ListNode temp=head;
+//        int[] count=new int[3];
+//        while(temp!=null){
+//            count[temp.data]++;
+//            temp=temp.next;
+//        }
+//        temp=head;
+//        for(int i=0;i<3;i++){
+//            while(count[i]>0){
+//                temp.data=i;
+//                temp=temp.next;
+//                count[i]--;
+//            }
+//        }
+//        return head;
+//    }
 }
---
+++ Striver_A2Z/src/step6_LinkedList/lec3_medium_problems_on_LL/p12_intersection_of_2LL.java
@@ -0,0 +1,71 @@
+package step6_LinkedList.lec3_medium_problems_on_LL;
+import java.util.HashSet;
+public class p12_intersection_of_2LL {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode t1=headA;
+        ListNode t2=headB;
+        while(t1!=t2){
+            t1=t1.next;
+            t2=t2.next;
+            if(t1==t2) return t1;
+            if(t1==null) t1=headB;
+            if(t2==null) t2=headA;
+        }
+        return t1;
+    }
+
+
+
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        ListNode tempA=headA;
+//        ListNode tempB=headB;
+//        int len1=0;
+//        int len2=0;
+//        while(tempA!=null){
+//            len1++;
+//            tempA=tempA.next;
+//        }
+//        while(tempB!=null){
+//            len2++;
+//            tempB=tempB.next;
+//        }
+//        if(len1<len2){
+//            return collisionPoint(headA,headB,len2-len1);
+//        }
+//        else{
+//            return collisionPoint(headB,headA,len1-len2);
+//        }
+//    }
+//    //arguments-----smaller head, larger head, difference between lengths;
+//    public ListNode collisionPoint(ListNode t1,ListNode t2,int d){
+//        while(d>0){
+//            d--;
+//            t2=t2.next;
+//        }
+//        while(t1!=t2){
+//            t1=t1.next;
+//            t2=t2.next;
+//        }
+//        return t1;
+//    }
+
+
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        ListNode tempA=headA;
+//        ListNode tempB=headB;
+//        HashSet<ListNode> set= new HashSet<>();
+//        while(tempA!=null){
+//            set.add(tempA);
+//            tempA=tempA.next;
+//        }
+//        while(tempB!=null){
+//            if(set.contains(tempB)){
+//                return tempB;
+//            }
+//            tempB=tempB.next;
+//        }
+//        return null;
+//    }
+}
+
