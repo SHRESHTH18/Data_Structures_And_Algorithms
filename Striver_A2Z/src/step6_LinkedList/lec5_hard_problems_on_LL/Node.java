2c553a77ff84222635d0a9d866891b34d8b85ca9
SHRESHTH18 <shreshth18062005@gmail.com>
2025-04-30 21:24:29 +0530

    regular

--- Striver_A2Z/src/step6_LinkedList/lec5_hard_problems_on_LL/Node.java
+++ Striver_A2Z/src/step6_LinkedList/lec5_hard_problems_on_LL/Node.java
@@ -4,6 +4,7 @@ public class Node {
     int data;
     Node next;
     Node bottom;
+    Node random;
     Node(int data){
         this.data=data;
     }
--- Striver_A2Z/src/step6_LinkedList/lec5_hard_problems_on_LL/p4_clone_a_LL.java
+++ Striver_A2Z/src/step6_LinkedList/lec5_hard_problems_on_LL/p4_clone_a_LL.java
@@ -1,4 +1,25 @@
 package step6_LinkedList.lec5_hard_problems_on_LL;
-
+import java.util.*;
 public class p4_clone_a_LL {
+
+    public Node copyRandomList(Node head){
+        HashMap<Node,Node> map = new HashMap<>();
+        Node temp= head;
+        Node newHead=head;
+        int count=0;
+        while(temp!=null){
+            Node dummy= new Node(temp.data);
+            if(count==0) {
+                newHead=dummy;
+                count++;
+            }
+            map.put(temp,dummy);
+            temp=temp.next;
+        }
+        for(Node node:map.keySet()){
+            map.get(node).next=map.get(node.next);
+            map.get(node).random=map.get(node.random);
+        }
+        return newHead;
+    }
 }
